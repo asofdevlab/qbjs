039a99a4a2a5efc4b7435a1466809881ae86ae12
ahmadpiee <ahmadpiee69@gmail.com>
2026-01-12 18:00:06 +0700

    finalize docs for core library

---
+++ apps/documentation/src/components/logo.tsx
@@ -0,0 +1,8 @@
+export function LogoImage() {
+	return (
+		<div className="flex gap-1 items-center">
+			<img src="/logo.png" alt="logo" className="size-7 rounded-full" />
+			<h1 className="text-xl font-semibold">qbjs</h1>
+		</div>
+	);
+}
--- apps/documentation/src/lib/layout.shared.tsx
+++ apps/documentation/src/lib/layout.shared.tsx
@@ -1,9 +1,11 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { LogoImage } from "@/components/logo";
 
 export function baseOptions(): BaseLayoutProps {
 	return {
 		nav: {
-			title: "qbjs",
+			title: <LogoImage />,
 		},
+		githubUrl: "https://github.com/asofdevlab/qbjs",
 	};
 }
